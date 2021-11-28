d7897c05fc0451ad0d9f1d18421f647f52fb1258
Kartik Singh Chuphal <64568294+kartik077-cr7@users.noreply.github.com>
2021-11-28 12:15:18 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,5 +121,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
